7d39896f84c117ca033efddb83a18dc866ee87ce
gatheeshanipulma <gatheesha.official@gmail.com>
2023-10-24 09:21:58 +0530

    ss save krnna

--- Scripts/savess.sh
+++ Scripts/savess.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-scrot '%Y-%m-%d-%s_screenshot_$wx$h.jpg' -e 'mv $f $$(xdg-user-dir Pictures/Screenshots/)'
+scrot '%Y-%m-%d-%s_screenshot_$wx$h.jpg' -e 'mv $f $$(xdg-user-dir Wallpapers/Screenshots/)'
 notify-send "Screenshot Saved"
